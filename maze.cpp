f7901728cd130c2bacc30759ac1bf00902e4e74e
ShiinaOrez <shiina_orez@qq.com>
2018-05-24 17:39:04 +0800

    xxx

---
+++ maze.cpp
@@ -0,0 +1,19 @@
+#include<cstdio>
+#include<iostream>
+#include<time.h>
+#include<>
+using namespace std;
+struct block{
+    bool u;//up
+    bool d;//down
+    bool l;//left
+    bool r;//right
+}
+void build_maze(int n){
+
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    build_maze(n);
+}
